1bf34cf61d1667b93d0e3c5689935b3f6a7563b0
Mouaad <Mouadouammou1@gmail.com>
2024-12-06 20:09:05 +0100

    first commit

---
+++ src/main/java/ma/projet/grpc/GrpcApplication.java
@@ -0,0 +1,13 @@
+package ma.projet.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grpc2Application.class, args);
+    }
+
+}
